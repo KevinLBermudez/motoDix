6bca3a8c5f0f7304ad734cb8fb9b178a0440d889
KevinLBermudez <Kevin.bermudez6084@unaula.edu.co>
2021-09-22 14:55:11 -0500

    Recursos y primer diseño

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace final_motoDix
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuPictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
