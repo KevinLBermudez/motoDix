33cbf4f4c8143815729206dbd1aefa0dc90fa56f
KevinLBermudez <Kevin.bermudez6084@unaula.edu.co>
2021-09-24 19:45:12 -0500

    formularios añadidos

---
+++ frmIdentifier.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace final_motoDix
+{
+    partial class frmIdentifier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmIdentifier));
+            this.bfpcbCustomer = new Bunifu.UI.WinForms.BunifuPictureBox();
+            this.bfpcbDriver = new Bunifu.UI.WinForms.BunifuPictureBox();
+            this.bflblMotorcyclist = new Bunifu.UI.WinForms.BunifuLabel();
+            this.bflblUser = new Bunifu.UI.WinForms.BunifuLabel();
+            this.btnMinimizar = new System.Windows.Forms.PictureBox();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bfpcbCustomer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bfpcbDriver)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bfpcbCustomer
+            // 
+            this.bfpcbCustomer.AllowFocused = false;
+            this.bfpcbCustomer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bfpcbCustomer.AutoSizeHeight = true;
+            this.bfpcbCustomer.BorderRadius = 90;
+            this.bfpcbCustomer.Image = ((System.Drawing.Image)(resources.GetObject("bfpcbCustomer.Image")));
+            this.bfpcbCustomer.IsCircle = true;
+            this.bfpcbCustomer.Location = new System.Drawing.Point(332, 70);
+            this.bfpcbCustomer.Name = "bfpcbCustomer";
+            this.bfpcbCustomer.Size = new System.Drawing.Size(180, 180);
+            this.bfpcbCustomer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bfpcbCustomer.TabIndex = 0;
+            this.bfpcbCustomer.TabStop = false;
+            this.bfpcbCustomer.Type = Bunifu.UI.WinForms.BunifuPictureBox.Types.Circle;
+            // 
+            // bfpcbDriver
+            // 
+            this.bfpcbDriver.AllowFocused = false;
+            this.bfpcbDriver.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bfpcbDriver.AutoSizeHeight = true;
+            this.bfpcbDriver.BorderRadius = 84;
+            this.bfpcbDriver.Image = ((System.Drawing.Image)(resources.GetObject("bfpcbDriver.Image")));
+            this.bfpcbDriver.IsCircle = true;
+            this.bfpcbDriver.Location = new System.Drawing.Point(70, 82);
+            this.bfpcbDriver.Name = "bfpcbDriver";
+            this.bfpcbDriver.Size = new System.Drawing.Size(168, 168);
+            this.bfpcbDriver.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bfpcbDriver.TabIndex = 1;
+            this.bfpcbDriver.TabStop = false;
+            this.bfpcbDriver.Type = Bunifu.UI.WinForms.BunifuPictureBox.Types.Circle;
+            // 
+            // bflblMotorcyclist
+            // 
+            this.bflblMotorcyclist.AllowParentOverrides = false;
+            this.bflblMotorcyclist.AutoEllipsis = false;
+            this.bflblMotorcyclist.CursorType = null;
+            this.bflblMotorcyclist.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
+            this.bflblMotorcyclist.ForeColor = System.Drawing.Color.White;
+            this.bflblMotorcyclist.Location = new System.Drawing.Point(83, 256);
+            this.bflblMotorcyclist.Name = "bflblMotorcyclist";
+            this.bflblMotorcyclist.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.bflblMotorcyclist.Size = new System.Drawing.Size(139, 29);
+            this.bflblMotorcyclist.TabIndex = 3;
+            this.bflblMotorcyclist.Text = "Motorcyclist";
+            this.bflblMotorcyclist.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.bflblMotorcyclist.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // bflblUser
+            // 
+            this.bflblUser.AllowParentOverrides = false;
+            this.bflblUser.AutoEllipsis = false;
+            this.bflblUser.CursorType = null;
+            this.bflblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
+            this.bflblUser.ForeColor = System.Drawing.Color.White;
+            this.bflblUser.Location = new System.Drawing.Point(392, 256);
+            this.bflblUser.Name = "bflblUser";
+            this.bflblUser.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.bflblUser.Size = new System.Drawing.Size(55, 29);
+            this.bflblUser.TabIndex = 4;
+            this.bflblUser.Text = "User";
+            this.bflblUser.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.bflblUser.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimizar.Image")));
+            this.btnMinimizar.Location = new System.Drawing.Point(535, 12);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(17, 18);
+            this.btnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnMinimizar.TabIndex = 13;
+            this.btnMinimizar.TabStop = false;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
+            this.btnCerrar.Location = new System.Drawing.Point(558, 12);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(18, 18);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnCerrar.TabIndex = 14;
+            this.btnCerrar.TabStop = false;
+            // 
+            // frmIdentifier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(54)))), ((int)(((byte)(66)))));
+            this.ClientSize = new System.Drawing.Size(586, 352);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnMinimizar);
+            this.Controls.Add(this.bflblUser);
+            this.Controls.Add(this.bflblMotorcyclist);
+            this.Controls.Add(this.bfpcbDriver);
+            this.Controls.Add(this.bfpcbCustomer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmIdentifier";
+            this.Text = "frmIdentifier";
+            ((System.ComponentModel.ISupportInitialize)(this.bfpcbCustomer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bfpcbDriver)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.UI.WinForms.BunifuPictureBox bfpcbCustomer;
+        private Bunifu.UI.WinForms.BunifuPictureBox bfpcbDriver;
+        private Bunifu.UI.WinForms.BunifuLabel bflblMotorcyclist;
+        private Bunifu.UI.WinForms.BunifuLabel bflblUser;
+        private System.Windows.Forms.PictureBox btnMinimizar;
+        private System.Windows.Forms.PictureBox btnCerrar;
+    }
+}
---
+++ frmRegister.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final_motoDix
+{
+    public partial class frmRegisterClient : Form
+    {
+        public frmRegisterClient()
+        {
+            InitializeComponent();
+        }
+
+        private void bflblCreateAccount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmRegister_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bftxtConfirmPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
