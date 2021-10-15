cf6d9ce24e36fa8fb093420f487373c789fa6f70
kevinLBermudez <kevin.bermudez6084@unaula.edu.co>
2021-10-15 14:52:43 -0500

    conexion

---
+++ Modelos/conexion.modelo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace final_motoDix.Modelos
+{
+    class conexion
+    {
+
+
+
+    }
+}
