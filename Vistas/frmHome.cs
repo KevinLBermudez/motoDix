dfe2d2eca57bb5b8005eb752cff49d3f2a12ead6
AlexisESuarez <ale.xsuuarez@gmail.com>
2021-11-11 19:22:25 -0500

    nose

--- Vistas/frmHome.cs
+++ Vistas/frmHome.cs
@@ -49,7 +49,15 @@ namespace final_motoDix.Vistas
 
         private void frmHome_Load(object sender, EventArgs e)
         {
-            AbrirFormInPanel<frmTravel>();
+            Form viaje = new frmTravel(infoPersona);
+            viaje.TopLevel = false;
+            viaje.FormBorderStyle = FormBorderStyle.None;
+            viaje.Dock = DockStyle.Fill;
+            panelContenedor.Controls.Add(viaje);
+            viaje.Show();
+            viaje.BringToFront();
+            
+
             bflblNombreUsuario.Text = infoPersona.FirstName +" " + infoPersona.SecondName + " " + infoPersona.Surname + " " + infoPersona.SecondSurname;
             bflblTipoUsuario.Text = infoPersona.RolName;
             bfpbImagenPerfil.ImageLocation = infoPersona.ProfilePicture;
@@ -78,7 +86,7 @@ namespace final_motoDix.Vistas
 
         private void bfbtnViajar_Click(object sender, EventArgs e)
         {
-            AbrirFormInPanel<frmTravel>();
+            //AbrirFormInPanel<frmTravel>();
         }
 
         private void ptbMinizar_Click(object sender, EventArgs e)
@@ -118,9 +126,9 @@ namespace final_motoDix.Vistas
             AbrirFormInPanel<frmAyuda>();
         }
 
-        private void panelHeader_Click(object sender, EventArgs e)
+        private void bfbtnVerSolicitudes_Click(object sender, EventArgs e)
         {
-
+            AbrirFormInPanel<frmSolicitudViajes>();
         }
     }
 }
